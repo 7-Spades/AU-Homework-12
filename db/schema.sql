621a65cedfed7fe738ee26fc6e0fed7c8341f397
7-Spades <edwin5106@gmail.com>
2020-03-27 14:45:39 -0500

    database setup

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burger(
+    Id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(40),
+    devoured BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY(Id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (NAME) VALUES ("Alvocado burger");
+INSERT INTO burger (NAME) VALUES ("Spicy Chicken burger");
+INSERT INTO burger (Name) VALUES ("Double Bacon burger");
