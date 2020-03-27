185ae4f5d15fa4f0e81b0ecf5a097291940bf31a
7-Spades <edwin5106@gmail.com>
2020-03-26 22:52:07 -0500

    established server routes

---
+++ controller/burgerController.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.router();
+
+const burger = require("../model/burger.js");
+
+router.get("/", function(req, res){
+    burger.all(function(data){
+        var burgerobj = {
+            burger: data
+        };
+        console.log(burgerobj);
+        res.render("index", burgerobj);
+    });
+});
+
+router.post("/api/:burger", function(req, res){
+    burger.create(["name", "devoured"],
+    [req.body.name, req.body.devoured],
+    function(result){
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burger/:id", function(req, res){
+    let condition = `id = ${req.prams.id}`;
+    console.log("condition", condition)
+    burger.update({
+        devoured: req.body.devoured
+    },
+    condition, function(result){
+        if(result.changedRow == 0){
+            return res.status(404).end()
+        } else{
+            res.status(200).end()
+        }
+    });
+});
+
+
+module.exports = router;
